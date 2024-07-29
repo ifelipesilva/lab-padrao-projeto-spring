53349ad83a4c79bf0a0dd603ca4812f29b1aee9c
ifelipesilva <ifelipesilvapro@gmail.com>
2024-07-29 17:06:35 -0300

    base do projeto

---
+++ src/main/java/one/digitalInovation/gof/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,32 @@
+package one.digitalInovation.gof.service.impl;
+
+import one.digitalInovation.gof.model.Cliente;
+import one.digitalInovation.gof.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
